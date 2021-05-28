d9f57ff9a542b2698d1f357245e2495e2df844f8
ace <ace2010shuxuemath@163.com>
2021-05-28 16:19:46 +0800

    项目初始化

---
+++ src/main/java/com/ace/acenote/AcenoteApplication.java
@@ -0,0 +1,13 @@
+package com.ace.acenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcenoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcenoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ace/acenote/AcenoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.acenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcenoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
